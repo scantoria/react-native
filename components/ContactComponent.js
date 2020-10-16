2cafeef4632fc78ea6e19ace1e7d969d26937dca
Stephen Cantoria <kstv@yahoo.com>
2020-10-16 16:13:19 -0400

    task 3

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
+function RenderContact(){
+    return(
+        <Card title="Contact Information">
+            <View>
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text>U.S.A.</Text>
+                <Text></Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+            </View>
+        </Card>
+    );
+}
 class Contact extends Component {
 
     static navigationOptions = {
-        title: 'Contact'
+        title: 'Contact Us'
     }
 
     render(){
-        return(
-            <View>
-                <Text>Contact Us</Text>
-            </View>
-        );
+        return <RenderContact />;
     }
 }
 
