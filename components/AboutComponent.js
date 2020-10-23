8ef7574e3ea4cee60faab12d06b50ebc8bd460b7
Stephen Cantoria <kstv@yahoo.com>
2020-10-22 22:28:49 -0400

    redux convertion complete

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import { FlatList, ScrollView} from 'react-native';
+import { FlatList, ScrollView, Text} from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
-import Mission from './MissionComponent';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return{
+        partners: state.partners
+    };
+};
+
+function Mission(){
+    return(
+        <Card title="Our Mission">
+            <Text style={{margin: 10}}>We present a curated database of the best campsites in the vast woods and backcountry 
+                of the World Wide Web Wilderness. We increase access to adventure for the public while 
+                promoting safe and respectful use of resources. The expert wilderness trekkers on our 
+                staff personally verify each campsite to make sure that they are up to our standards. 
+                We also present a platform for campers to share reviews on campsites they have visited 
+                with each other.
+            </Text>
+        </Card>
+    );
+}
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
+
     static navigationOptions = {
         title: 'About Us'
     }
@@ -21,7 +36,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
                 />
             );
         };
@@ -30,7 +45,7 @@ class About extends Component {
                 <Mission />
                 <Card title="Community Partners">
                     <FlatList
-                        data={this.state.partners}
+                        data={this.props.partners.partners}
                         renderItem={renderPartners}
                         keyExtractor={item => item.id.toString()}
                     />
@@ -40,4 +55,4 @@ class About extends Component {
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
