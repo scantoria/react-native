b3d04a24464dea8a4584d057c06bb3af7c437f24
Stephen Cantoria <kstv@yahoo.com>
2020-10-29 22:14:52 -0400

    animatable Directory

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 function RenderContact(){
     return(
-        <Card title="Contact Information" wrapperStyle={{margin: 20}}>
-            <View>
-                <Text>1 Nucamp Way</Text>
-                <Text>Seattle, WA 98001</Text>
-                <Text style={styles.lineBreak}>U.S.A.</Text>
-                <Text>Phone: 1-206-555-1234</Text>
-                <Text>Email: campsites@nucamp.co</Text>
-            </View>
-        </Card>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+            <Card title="Contact Information" wrapperStyle={{margin: 20}}>
+                <View>
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={styles.lineBreak}>U.S.A.</Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+                </View>
+            </Card>
+        </Animatable.View>
     );
 }
 class Contact extends Component {
